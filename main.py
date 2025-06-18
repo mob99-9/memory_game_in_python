9ed0e6bd9cc06ddf094354635076a4d5ff7cdaea
mob99-9 <jamesorozco20050@gmail.com>
2025-06-18 23:13:09 +0800

    added entry, submit, check functions

--- main.py
+++ main.py
@@ -10,13 +10,29 @@ title = Label(main_menu, text="Memory", font=(50))
 title.pack()
 
 class GameMaster(Arbiter):
-    def Hello(self):
-        pass
 
+    def show_code(self):
+        code = Label(main_menu, text=f"{game.randomizer()}", fg="BLACK", font=(300))
+        code.place(x=330, y=90)
+
+    def user_entry(self):
+        global ask_user
+        ask_user = Entry(main_menu)
+        ask_user.place(x=310, y=280)
+
+    def user_submit(self):
+        submit_button = Button(main_menu, text="Submit", command=game.check_user)
+        submit_button.place(x=340,y=300)
+
+    def check_user(self):
+        user_code = ask_user.get()
+        game.scorer(user_code)
+        
 game = GameMaster()
 
-code = Label(main_menu, text=f"{game.randomizer()}", fg="BLACK", font=(300))
-code.place(x=330, y=90)
+game.show_code()
+game.user_entry()
+game.user_submit()
 
 #show timer and blocker 
 
--- system.py
+++ system.py
@@ -2,6 +2,7 @@ import random
 import time
 class Arbiter():
     def randomizer(self):
+        global shuffled_code
         code = ""
         alphabets = ["A","a","B","b","C","c","D","d","E","e","F","f","G","g",
                     "H","h","I","i","J","j","K","k","L","l","M","m","N","n",
@@ -10,11 +11,11 @@ class Arbiter():
         numbers = ["1", "2", "3", "4", "5","6","7","8","9","0"]
 
         for counter in range(0,6):
-            random_letter = alphabets[random.randint(1,51)]
+            random_letter = alphabets[random.randint(0,51)]
             code += random_letter
         
         for counter_number in range(0,2):
-            random_number = numbers[random.randint(1,9)]
+            random_number = numbers[random.randint(0,9)]
             code += random_number
 
         shuffled_code = "".join(random.sample(code, len(code)))
@@ -27,10 +28,9 @@ class Arbiter():
 
 #create score shower
     def scorer(self, guess):
-        global score
         score = 0
         if guess == shuffled_code:
             score+= 1
         else:
             None
-        
+        print(score)
